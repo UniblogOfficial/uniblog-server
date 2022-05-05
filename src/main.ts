0b92e9cd4b42c1b497a9e0fa8e19cfda8600a652
Krankenkopf <petricowork@gmail.com>
2022-05-05 17:50:58 +0300

    cors: add options

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
-import { ValidationPipe } from './core/pipes/validation.pipe';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const whitelist = ['https://uniblog-online.netlify.app', 'http://localhost:3000'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      console.log('Allowed cors for: ', origin);
+      callback(null, true);
+    } else {
+      console.log('Blocked cors for: ', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  // allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+  credentials: true,
+};
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   console.log(process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
