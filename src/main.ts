920859d58055cc2f0bfe1283071d7fa413510ed9
Krankenkopf <petricowork@gmail.com>
2022-05-08 17:18:14 +0300

    cors: add extra hosts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-const whitelist = ['https://uniblog-online.netlify.app', 'http://localhost:3000'];
+const whitelist = [
+  'https://uniblog-online.netlify.app',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || process.env.NODE_ENV === 'development') {
       console.log('Allowed cors for: ', origin);
       callback(null, true);
     } else {
