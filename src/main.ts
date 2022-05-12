cd0c9fdf505a459a7fc80462a1a97440b8e21130
Krankenkopf <petricowork@gmail.com>
2022-05-12 12:26:05 +0300

    cors: fix same origin request error

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -11,12 +12,12 @@ const whitelist = [
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || process.env.NODE_ENV === 'development') {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       console.log('Allowed cors for: ', origin);
       callback(null, true);
     } else {
       console.log('Blocked cors for: ', origin);
-      callback(new Error('Not allowed by CORS'));
+      callback(new HttpException('Not allowed by CORS', HttpStatus.FORBIDDEN));
     }
   },
   // allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
@@ -35,7 +36,6 @@ async function start() {
     .setTitle('Uniblog Server')
     .setDescription('Uniblog REST API Documentation')
     .setVersion('0.1.0')
-    .addTag('krank')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
